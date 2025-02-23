b04ddd3c65fb8716712573f12896750fcb751483
Kevin van den Hoek <kevinvandenhoek@gmail.com>
2025-02-23 10:32:06 +0100

    add register macro

--- Tests/DependenciesMacrosPluginTests/RegisterMacroTests.swift
+++ Tests/DependenciesMacrosPluginTests/RegisterMacroTests.swift
@@ -5,3 +5,38 @@
 //  Created by Kevin van den Hoek on 23/02/2025.
 //
 
+import DependenciesMacrosPlugin
+import MacroTesting
+import XCTest
+
+final class RegisterMacroTests: BaseTestCase {
+  override func invokeTest() {
+    withMacroTesting(
+      // isRecording: true,
+      macros: [RegisterMacro.self]
+    ) {
+      super.invokeTest()
+    }
+  }
+  
+  func testBasics() {
+    assertMacro {
+        """
+        #register(BookingStore, RealBookingStore())
+        """
+    } expansion: {
+        """
+        public extension DependencyValues {
+            private enum BookingStoreDependencyKey: DependencyKey {
+                static var liveValue: BookingStore = RealBookingStore()
+            }
+            
+            var bookingStore: BookingStore {
+                get { self[BookingStoreDependencyKey.self] }
+                set { self[BookingStoreDependencyKey.self] = newValue }
+            }
+        }
+        """
+    }
+  }
+}
